4f63633384ff77729a66a9f125aea57a2b3f3c0c
moses kintu <engineerkintu@gmail.com>
2019-12-12 20:54:34 +0300

    Add files via upload
    File to for holding user data

---
+++ users.py
@@ -0,0 +1,24 @@
+def users():
+	users = [
+		{	'id':'1',
+			'username':'moze',
+			'email':'engkmos@gmail.com',
+			'password':'123',
+		},
+		{	'id':'2',
+			'username':'willy',
+			'email':'willy@gmail.com',
+			'password':'abc',
+		},
+		{	'id':'3',
+			'username':'rosy',
+			'email':'rosy@hotmail.com',
+			'password':'222',
+		},
+		{	'id':'4',
+			'userrname':'daphy',
+			'email':'daphy@yahoo.com',
+			'password':'bbb',
+		},
+	]
+	return users
