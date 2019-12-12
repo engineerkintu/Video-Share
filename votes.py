59feb4f71bba8ee6bb7241bbeb3ffaa2ab17210e
moses kintu <engineerkintu@gmail.com>
2019-12-12 20:55:31 +0300

    Add files via upload
    File for storing voter data in a given instance

---
+++ votes.py
@@ -0,0 +1,19 @@
+def votes():
+	votes = [
+		{
+			'id':'1',
+			'movie_id':'2',
+			'voted_by':'moze',
+		},
+		{
+			'id':'2',
+			'movie_id':'2',
+			'voted_by':'daphy',
+		},
+		{
+			'id':'3',
+			'movie_id':'3',
+			'voted_by':'rosy',
+		},
+	]
+	return votes
